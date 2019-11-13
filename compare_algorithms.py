95eaeb7368395bad085f51bd555d45c5c059541d
Azimovv <suggzugg7375@gmail.com>
2019-11-13 13:47:35 -0500

    comparing differing machine learning algorithms from sklearn

---
+++ compare_algorithms.py
@@ -0,0 +1,45 @@
+# Comparing different ML algorithms
+from pandas import read_csv
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+# Split-out validation dataset
+array = dataset.values
+x = array[:, 0:4]
+y = array[:, 4]
+x_train, x_validation, y_train, y_validation = train_test_split(x, y, test_size=0.20, random_state=1)
+
+# Spot check algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1)
+    cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print(f"{name}: {cv_results.mean()} ({cv_results.std()})")
+
+# compare algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
